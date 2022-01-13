6afcaf29e045b5fa34476f7444219e2cf4b061d3
Yunus-Fouad <yunus.fouad@themindgym.com>
2022-01-13 15:00:48 +0000

    inital commit

---
+++ exercism/python/ghost-gobble-arcade-game/arcade_game.py
@@ -0,0 +1,39 @@
+def eat_ghost():
+    """
+
+    :param power_pellet_active: bool - does the player have an active power pellet?
+    :param touching_ghost:  bool - is the player touching a ghost?
+    :return: bool
+    """
+    pass
+
+
+def score():
+    """
+
+    :param touching_power_pellet: bool - does the player have an active power pellet?
+    :param touching_dot:  bool - is the player touching a dot?
+    :return: bool
+    """
+    pass
+
+
+def lose():
+    """
+
+    :param power_pellet_active: bool - does the player have an active power pellet?
+    :param touching_ghost: bool - is the player touching a ghost?
+    :return: bool
+    """
+    pass
+
+
+def win():
+    """
+
+    :param has_eaten_all_dots: bool - has the player "eaten" all the dots?
+    :param power_pellet_active: bool - does the player have an active power pellet?
+    :param touching_ghost:  bool - is the player touching a ghost?
+    :return: bool
+    """
+    pass
