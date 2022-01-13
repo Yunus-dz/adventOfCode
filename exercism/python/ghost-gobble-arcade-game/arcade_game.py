0bf228041f0496e1efe2dd15096e4246eec4e0ac
Yunus-Fouad <yunus.fouad@themindgym.com>
2022-01-13 16:29:28 +0000

    update on code commit

--- exercism/python/ghost-gobble-arcade-game/arcade_game.py
+++ exercism/python/ghost-gobble-arcade-game/arcade_game.py
@@ -1,34 +1,43 @@
-def eat_ghost():
+def eat_ghost(power_pellet_active, touching_ghost):
     """
 
     :param power_pellet_active: bool - does the player have an active power pellet?
     :param touching_ghost:  bool - is the player touching a ghost?
     :return: bool
     """
-    pass
+    if power_pellet_active and touching_ghost:
+        return True
+    else:
+        return False
 
 
-def score():
+def score(touching_power_pellet, touching_dot):
     """
 
     :param touching_power_pellet: bool - does the player have an active power pellet?
     :param touching_dot:  bool - is the player touching a dot?
     :return: bool
     """
-    pass
+    if touching_power_pellet or touching_dot:
+        return True
+    else:
+        return False
 
 
-def lose():
+def lose(power_pellet_active, touching_ghost):
     """
 
     :param power_pellet_active: bool - does the player have an active power pellet?
     :param touching_ghost: bool - is the player touching a ghost?
     :return: bool
     """
-    pass
+    if not power_pellet_active and touching_ghost:
+        return True
+    else:
+        return False
 
 
-def win():
+def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
     """
 
     :param has_eaten_all_dots: bool - has the player "eaten" all the dots?
@@ -36,4 +45,7 @@ def win():
     :param touching_ghost:  bool - is the player touching a ghost?
     :return: bool
     """
-    pass
+    if has_eaten_all_dots and (not power_pellet_active and touching_ghost):
+        return True
+    else:
+        return false
