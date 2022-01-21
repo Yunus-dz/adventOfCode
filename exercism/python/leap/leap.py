14cf661bfd92e88c74df9772840c790c41b83b9d
Yunus-Fouad <yunus.fouad@themindgym.com>
2022-01-21 14:46:09 +0000

    final commit leap

--- exercism/python/leap/leap.py
+++ exercism/python/leap/leap.py
@@ -1,2 +1,7 @@
 def leap_year(year):
-    pass
+    if year%4==0 and year%100 != 0:
+        return True
+    elif year%4==0 and year%100 == 0 and year%400 == 0 :
+        return True
+    else:
+        return False
