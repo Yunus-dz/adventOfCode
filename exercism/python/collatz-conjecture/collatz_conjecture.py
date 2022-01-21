155453a3bdf73c7db2919ae587cd3a3c5980f69e
Yunus-Fouad <yunus.fouad@themindgym.com>
2022-01-21 14:53:08 +0000

    final commit collatz

--- exercism/python/collatz-conjecture/collatz_conjecture.py
+++ exercism/python/collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,13 @@
 def steps(number):
-    pass
+    if number <= 0:
+        raise ValueError("Only positive numbers are allowed")
+    count_steps = 0
+    while number != 1:
+        # Proceso de numero par
+        if number % 2 == 0:
+            number = number / 2
+            count_steps += 1
+        else:
+            number = (3*number) + 1
+            count_steps += 1
+    return count_steps
