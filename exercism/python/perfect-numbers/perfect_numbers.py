f7262dec03186e8ebbd73a58c6701b879b0b9e9f
Yunus-Fouad <yunus.fouad@themindgym.com>
2022-01-25 09:48:50 +0000

    final commit perfect_numbers

--- exercism/python/perfect-numbers/perfect_numbers.py
+++ exercism/python/perfect-numbers/perfect_numbers.py
@@ -4,4 +4,14 @@ def classify(number):
     :param number: int a positive integer
     :return: str the classification of the input integer
     """
-    pass
+    sum = 0
+    if number < 1:
+        raise ValueError("Classification is only possible for positive integers.")
+    for value in range(1, (number // 2) + 1):
+        if number % value == 0:
+            sum += value
+    if number == sum:
+        return "perfect"
+    elif number > sum:
+        return "deficient"
+    return "abundant"
