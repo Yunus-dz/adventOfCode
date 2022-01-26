cf29a92d37d21d724ef3f8943b3b589e8bbaf025
Yunus-Fouad <yunus.fouad@themindgym.com>
2022-01-26 09:17:45 +0000

    initial commit largest_series_product

--- exercism/python/largest-series-product/largest_series_product.py
+++ exercism/python/largest-series-product/largest_series_product.py
@@ -1,2 +1,23 @@
 def largest_product(series, size):
-    pass
+            # span of numbers is longer than number series
+    if size > len(str(series)):
+        raise ValueError("span must be smaller than string length")
+    if int(size) < 0:
+        # span of number is zero or negative
+        raise ValueError("span must be greater than zero")
+    # series includes non-number input
+    for item in series:
+        if item.isdigit():
+            continue
+        else:
+            raise ValueError("digits input must only contain digits")
+
+    largest = 0
+    for i in range(len(series) - size + 1):
+        elements = series[i:i + size]
+        product = 1
+        for element in elements:
+            product = product * int(element)
+        if product > largest:
+            largest = product
+    return largest
