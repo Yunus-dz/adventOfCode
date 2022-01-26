226ccf0a6fbeb8414b165b408000dbe2fb2089eb
Yunus-Fouad <yunus.fouad@themindgym.com>
2022-01-26 09:20:48 +0000

    final commit atbash

--- exercism/python/atbash-cipher/atbash_cipher.py
+++ exercism/python/atbash-cipher/atbash_cipher.py
@@ -1,2 +1,11 @@
 def cleanup(text):
-    return
+    return ''.join(a.lower() for a in text if a.isalnum())
+
+
+def encode(text):
+    text = cleanup(text)
+    text = text.translate(text.maketrans(plain,cipher))
+    return ' '.join(text[i:i+5] for i in range(0, len(text), 5))
+
+def decode(text):
+    return text.translate(text.maketrans(plain,cipher)).replace(" ","")
